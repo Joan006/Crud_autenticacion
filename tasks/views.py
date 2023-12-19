ed320332cfdd43a4c150e5432ca59005c6661e85
JoanMdv <joan_martinez.olivares@hotmail.com>
2023-12-18 23:11:44 -0600

    Primer avance - creacion de signup - con comprobacion sobre su registro

---
+++ tasks/views.py
@@ -0,0 +1,49 @@
+from unicodedata import name
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login
+from django.db import IntegrityError
+
+
+def home(request):
+    return render(request, "home.html")
+
+
+def signup(request):
+    if request.method == "GET":
+        print("Enviando Formulario")
+    else:
+        if request.POST["password1"] == request.POST["password2"]:
+            try:
+                user = User.objects.create_user(
+                    username=request.POST["username"],
+                    password=request.POST["password1"],
+                )
+                user.save()
+                login(request, user)
+                return redirect("tasks")
+            except IntegrityError:
+                return render(
+                    request,
+                    "signup.html",
+                    {"form": UserCreationForm, "error": "Username already exists"},
+                )
+        return render(
+            request,
+            "signup.html",
+            {"form": UserCreationForm, "error": "Password do not match"},
+        )
+
+    return render(
+        request,
+        "signup.html",
+        {
+            # Se crea un formulario importado
+            "form": UserCreationForm
+        },
+    )
+
+
+def tasks(request):
+    return render(request, "tasks.html")
